ba7d1fb568ec23f4e3dc3f759ee5677dd3abe5e8
QuangNM <qnmhust.research@gmail.com>
2026-01-31 03:27:47 +0700

    Adding custom banner - fix MIT check sum

--- recipes-core/qnm-banner/files/qnm-banner.sh
+++ recipes-core/qnm-banner/files/qnm-banner.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-echo "
+cat << 'EOF'
    ____  _   _ __  __ 
   / __ \| \ | |  \/  |
  | |  | |  \| | \  / |
  | |  | | . ` | |\/| |
  | |__| | |\  | |  | |
   \___\_\_| \_|_|  |_|
-                      
-  Uptime     : $(uptime)
-  Hostname   : $(hostname)
-  Disk Usage : $(df -h | awk '/\/$/ {print $3 " used of " $2}')
-  Memory     : $(free | awk '/Mem:/ {printf "%.1fM used of %.1fM", $3/1024, $2/1024}')
-" 
+EOF
+
+echo "  Uptime     : $(uptime)"
+echo "  Hostname   : $(hostname)"
+echo "  Disk Usage : $(df -h | awk '/\/$/ {print $3 " used of " $2}')"
+echo "  Memory     : $(free | awk '/Mem:/ {printf "%.1fM used of %.1fM", $3/1024, $2/1024}')"
--- recipes-core/qnm-banner/qnm-banner.bb
+++ recipes-core/qnm-banner/qnm-banner.bb
@@ -5,7 +5,7 @@ SRC_URI = "file://qnm-banner.sh file://COPYING.MIT"
 
 DESCRIPTION = "QNM login banner with system info"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://COPYING.MIT;md5=04026fa0bcdc69bf6b9e72d6c96e25ae"
 
 S = "${WORKDIR}"
 
